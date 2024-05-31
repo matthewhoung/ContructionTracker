d871ce15d037f86e1332b33ec1178f2b3b48dda5
DESKTOP-FTVMTJF\user <matthewhoung@gmail.com>
2024-05-31 18:14:30 +0800

    taken off the unused Dtos

--- Application/DTOs/OrderFromWorkerDto.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Application.DTOs
-{
-    public class OrderFromWorkerDto
-    {
-        public int OrderFormId { get; set; }
-        public int WorkerTypeId { get; set; }
-        public int WorkerTeamId { get; set; }
-    }
-}
